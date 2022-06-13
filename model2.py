477fa35f2b089928b9ce656839a42e0947bcbedf
nurkbts <snrkabatas@hotmail.com>
2022-06-13 16:09:47 +0300

    Create model2.py

---
+++ model2.py
@@ -0,0 +1,35 @@
+# Importing necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# Importing the dataset
+data = pd.read_csv('iris.csv')
+
+# Dictionary containing the mapping
+variety_mappings = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+
+# Encoding the target variables to integers
+data = data.replace(['Setosa', 'Versicolor' , 'Virginica'],[0, 1, 2])
+
+X = data.iloc[:, 0:-1] # Extracting the independent variables
+y = data.iloc[:, -1] # Extracting the target/dependent variable
+
+neigh = KNeighborsClassifier(n_neighbors=3) # Initializing the Logistic Regression model
+neigh.fit(X, y) # Fitting the model
+
+# Function for classification based on inputs
+def classify(a, b, c, d):
+    arr = np.array([a, b, c, d]) # Convert to numpy array
+    arr = arr.astype(np.float64) # Change the data type to float
+    query = arr.reshape(1, -1) # Reshape the array
+    prediction = variety_mappings[neigh.predict(query)[0]] # Retrieve from dictionary
+    return prediction # Return the prediction
+
+def classify2(a, b, c, d):
+    arr = np.array([a, b, c, d]) # Convert to numpy array
+    arr = arr.astype(np.float64) # Change the data type to float
+    query = arr.reshape(1, -1) # Reshape the array
+    prediction = variety_mappings[neigh.predict(query)[0]] # Retrieve from dictionary
+    return "a" # Return the prediction
